49c9983f1f68e8b16e8b01022b4ff7f2d44c1724
guuhx97 <gustavorobertotb@outlook.com>
2020-01-14 20:37:13 -0300

    🔙 Application backend development completed

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import Dev from '../models/Dev';
+import parseStringAsArray from '../utils/parseStringAsArray';
+
+module.exports = {
+  async index(req, res) {
+    const devs = await Dev.find();
+    return res.json(devs);
+  },
+
+  async store(req, res) {
+    const { github_username, techs, latitude, longitude } = req.body;
+    let dev = await Dev.findOne({ github_username });
+    if (!dev) {
+      const response = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
+      // eslint-disable-next-line no-undef
+      const { name = login, avatar_url, bio } = response.data;
+      const techsArray = parseStringAsArray(techs);
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      };
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location,
+      });
+    }
+    return res.json(dev);
+  },
+};
